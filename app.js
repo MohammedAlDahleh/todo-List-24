b64a531a546f5421ad1cb4534eb88172eff8fe5f
Mohammed AlDahleh <mohammedd993@gmail.com>
2024-05-03 17:28:06 +0000

    JS part

--- app.js
+++ app.js
@@ -5,14 +5,19 @@ var userName = prompt(`Please enter his/her name.`);
 console.log(`Hallo ${userName} :)`);
 var age = prompt(`Please enter his/her age.`);
 var wMessage = confirm(`${userName} do you want to skip Welcoming Message? `);
-var x = " ";
+var yourTitel = " ";
 
 // This section for Gender of Customer.
 
-(gender.toLowerCase() === "male") ? console.log(`Hallo ${userName} your Gender is : Male`) :
-    (gender.toLowerCase() === "female") ? console.log(`Hallo ${userName} your Gender is : Female`) :
-        console.log(`Hallo ${userName}, Please enter a correct answer (male) OR (female)`)
-        alert(`Hallo ${userName}, Please enter a correct answer (male) OR (female)`);
+if (gender.toLowerCase() === "male") {
+
+    console.log(`Hallo ${userName} your Gender is : Male`);
+} else if (gender.toLowerCase() === "female") {
+    console.log(`Hallo ${userName} your Gender is : Female`)
+} else {
+    console.log(`Hallo ${userName}, Please enter a correct answer (male) OR (female)`)
+    alert(`Hallo ${userName}, Please enter a correct answer (male) OR (female)`);
+}
 
 /*if (gender.toLowerCase() === "his") {
 console.log(`Hallo ${userName} your Gender is : Male`);
@@ -27,11 +32,70 @@ console.log(`Hallo ${userName} your Gender is : Femal`);
 
 if (wMessage === false) {
     if (gender === "male") {
-        x = "Mr";
-        alert(`Welcome ${x} ${userName}`);
+        yourTitel = "Mr";
+        alert(`Welcome ${yourTitel} ${userName}`);
 
     } else {
-        x = "Ms";
-        alert(`Welcome ${x} ${userName}`);
+        yourTitel = "Ms";
+        alert(`Welcome ${yourTitel} ${userName}`);
     }
 }
+
+let kursHTML = prompt("Are you Familiar with HTML yes/no?")
+let kursCSS = prompt("Are you Familiar with CSS yes/no?")
+let kursJS = prompt("Are you Familiar with JS yes/no?")
+
+let storeAnswer = [];
+
+function kHTML() {
+}
+if (kursHTML.toLowerCase() === "yes" || kursHTML.toLowerCase() === "no") {
+    storeAnswer.push(kursHTML);
+} else {
+    storeAnswer.push("invalid");
+}
+ 
+kHTML();
+
+function kCSS() {
+    
+    if (kursCSS.toLowerCase() === "yes" || kursCSS.toLowerCase() === "no") {
+        storeAnswer.push(kursCSS);
+    } else {
+        storeAnswer.push("invalid");
+    }
+}
+
+kCSS();
+
+function kJS() {
+    
+    if (kursJS.toLowerCase() === "yes" || kursJS.toLowerCase() === "no") {
+        storeAnswer.push(kursJS);
+    } else {
+        storeAnswer.push("invalid");
+    }
+}
+
+kJS();
+
+function printAnswer() {
+    return storeAnswer;
+};
+
+function print(ans) {
+    
+    for (let i = 0; i < ans.length; i++) {
+        console.log(ans[i]);
+    }
+}
+print(storeAnswer); 
+
+printAnswer();
+console.log(printAnswer());
+
+
+
+
+
+
